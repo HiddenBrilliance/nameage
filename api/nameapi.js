52ed436b6383332d998b063a7a25dcec05d296bd
Aaron Alexander <aaronralexanderbusiness@gmail.com>
2022-10-27 06:41:48 -0500

    An update to the Javascript
    In this commit I updated the javascript to show me the age value for a given name and used .InnerHTML to display the age variable.

--- api/nameapi.js
+++ api/nameapi.js
@@ -1,32 +1,24 @@
 const url = 'https://api.agify.io?name=';
 
-
-const getAge = async (name) => {
-    const response = await fetch(url + name);
-    return response.json();
-}
-
-
-const handleClickSubmit = () => {
-    const nameInput = document.querySelector('#first-name')
-    console.log(nameInput.value)
-    getAge(nameInput.value)
-
-}
-
-const displayAge = () => {
-    const container = document.querySelector('#age-display')
-    console.log(container)
-    container.innerHTML = `<h1> ${value} </h1>`
-
-}
-
-
-
 const initializeApp = () => {
     const submitButton = document.querySelector('#submit')
     submitButton.addEventListener('click', handleClickSubmit)
-    container.adde
 }
+const getAge = async (name) => {
+    const response = await fetch(url + name);
+    return await response.json();
+}
+
+const handleClickSubmit = async () => {
+    const nameInput = document.querySelector('#first-name')
+    const response = await getAge(nameInput.value)
+    displayAge(response.age)
+}
+
+const displayAge = (age) => {
+    const container = document.getElementById('age-display')
+    container.innerHTML = `<h1>${age}</h1>`
+}
+
 
 initializeApp()
